2bec36d646ddfcc8ea50a7299c74e6b392606002
IrisaZhou <93854320+IrisaZhou@users.noreply.github.com>
2026-04-29 18:22:01 +0000

    Deploying to gh-pages from @ IrisaZhou/IrisaZhou.github.io@63b2692a49e808e75e366b81160e4e4db15f2905 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "IrisaZhou/personal-website",
+    "data-repo": "IrisaZhou/IrisaZhou.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
